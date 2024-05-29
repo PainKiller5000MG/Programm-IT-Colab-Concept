a8bbf8ef3131610b70426f241c250b9facad1a37
PainKillerCodes <163454050+PainKiller5000MG@users.noreply.github.com>
2024-05-29 19:11:14 +0530

    Revert "fixing auth styes"
    This reverts commit 3a67a3c956a8f654ce561b3bda1a1c38fa004f5a.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import type { Metadata } from "next";
-
+import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 import "../globals.css";
 
-
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Auth",
@@ -25,7 +25,7 @@ export default function RootLayout({
       }}
     >
       <html lang='en'>
-        <body className={` flex justify-center bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
       </html>
     </ClerkProvider>
   );
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp className="m-auto" />;
+  return <SignUp />;
 }
